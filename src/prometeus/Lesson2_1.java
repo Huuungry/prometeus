a5d70b5afe6c84ca6d129f92f5ea90fbc9c0119b
YuriJ Litvin <YuriJfake@gmail.com>
2017-03-02 20:08:12 +0200

    updated

--- src/prometeus/Lesson2_1.java
+++ src/prometeus/Lesson2_1.java
@@ -3,8 +3,25 @@ package prometeus;
 public class Lesson2_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double a = 3;
+		double b = 2.5;
+		double c = -0.5;
+
+		double d, x1, x2;
+		d = b * b - 4 * a * c;
+		if (a == 0 || d < 0) {
+			System.out.println("x1=");
+			System.out.println("x2=");
+		}
+
+		else if (d >= 0) {
+			x1 = (-b + Math.sqrt(d)) / (2 * a);
+			x2 = (-b - Math.sqrt(d)) / (2 * a);
+			System.out.println("x1=" + x1);
+			System.out.println("x2=" + x2);
+			
+			System.out.println(a*x2*x2+b*x2+c);
+		}
 	}
-
 }
