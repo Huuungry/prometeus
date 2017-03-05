b67b1d8f03531b977a4b3cba215a7d98504236ba
YuriJ Litvin <YuriJfake@gmail.com>
2017-03-05 09:21:29 +0200

    Changes after revert

--- src/prometeus/Lesson3_4.java
+++ src/prometeus/Lesson3_4.java
@@ -1,11 +1,30 @@
 package prometeus;
 
 public class Lesson3_4 {
-	
-	
-	
-	int data[] = { 3, 6, 7, 10, 34, 56, 60 };
-	int numberToFind = 10;
-	
-	
+
+	public static void main(String[] args) {
+
+		int data[] = { 3, 6, 7, 10, 34, 56, 60 };
+		int numberToFind = 10;
+
+		int lenght = data.length;
+		int left = 0;
+		int right = lenght - 1;
+
+		while (left <= right && left >= 0 && right >= 0) {
+			int middle = left + (right - left) / 2;
+			if (numberToFind == data[middle]) {
+				System.out.println(middle);
+				break;
+			} else if (numberToFind > data[middle]) {
+				left = middle + 1;
+			} else if (numberToFind < data[middle]) {
+				right = middle - 1;
+			}
+		}
+
+		if (left > right || left < 0 || right < 0) {
+			System.out.println(-1);
+		}
+	}
 }
