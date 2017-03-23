93074001f531e3d7458b9291be96db84ba8c5796
YuriJ Litvin <YuriJfake@gmail.com>
2017-03-23 07:24:34 +0200

    more then 10 changes

--- src/stage6/Sendbox.java
+++ src/stage6/Sendbox.java
@@ -4,7 +4,7 @@ public class Sendbox {
 
 	public static void main(String[] args) {
 
-		String str1 = "aaabbbmmmcccc";
+		String str1 = "aaabbbbbbbbbbbbmmmcccc";
 		String result = new String();
 		int numberOfRepeated = 1;
 		for (int i = 0; i < str1.length(); i++) {
@@ -12,20 +12,25 @@ public class Sendbox {
 			if (i < str1.length()-1 && str1.charAt(i) != str1.charAt(i + 1)) {
 				// System.out.println(numberOfRepeated);
 				result = result.concat(Character.toString(str1.charAt(i)));
-				if (numberOfRepeated != 1){
+				if (numberOfRepeated > 1 && numberOfRepeated < 10){
 					result = result.concat(Integer.toString(numberOfRepeated));
 					numberOfRepeated=1;
 				}
+			
 			} else if (i == str1.length()-1) {
 				result = result.concat(Character.toString(str1.charAt(i)));
 				if (numberOfRepeated != 1){
 					result = result.concat(Integer.toString(numberOfRepeated));
 				}
 			}
-			else {
+			else if (numberOfRepeated<9) {
 				numberOfRepeated++;
 			}
-
+			else{
+				result = result.concat(Character.toString(str1.charAt(i)));
+				result = result.concat(Integer.toString(numberOfRepeated));
+				numberOfRepeated=1;
+			}
 		}
 		System.out.println(result);
 	}
